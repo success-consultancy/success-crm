4fd8cf23ea764857c6af880fff5225bac3ab863a
pokhrelgopal <pokhrelgopal27@gmail.com>
2025-07-31 08:38:21 +0545

    feat: add document and logout feature

--- src/app/(auth)/reset-password/_components/password-change-success.tsx
+++ src/app/(auth)/reset-password/_components/password-change-success.tsx
@@ -1,16 +1,15 @@
-"use client";
-import React from "react";
+'use client';
+import React from 'react';
 // import { useRouterOriginal } from '@/lib/navigation';
 
-import { ROUTES } from "@/config/routes";
-import Button from "@/components/atoms/button";
-import { useRouterOriginal } from "@/lib/navigation";
+import { ROUTES } from '@/config/routes';
+import Button from '@/components/atoms/button';
+import { useRouterOriginal } from '@/lib/navigation';
 // import Button from '@/components/common/button';
 // import { BrandLogoNav } from './brand-logo-nav';
 // import { SuccessIllustration } from '@/assets/illustrations';
-type Props = {};
 
-const PasswordChangeSuccess = (props: Props) => {
+const PasswordChangeSuccess = () => {
   const currentYear = new Date().getFullYear();
   const router = useRouterOriginal();
 
@@ -20,23 +19,14 @@ const PasswordChangeSuccess = (props: Props) => {
 
       {/* <SuccessIllustration className="mt-20" /> */}
 
-      <h5 className="text-center text-h2 text-[2.1875rem] text-state-success-base mt-11">
-        Success
-      </h5>
-      <p className="text-b1 text-center text-content-body mt-4">
-        You have successfully changed your password.
-      </p>
+      <h5 className="text-center text-h2 text-[2.1875rem] text-state-success-base mt-11">Success</h5>
+      <p className="text-b1 text-center text-content-body mt-4">You have successfully changed your password.</p>
 
-      <Button
-        className="self-center mt-10"
-        onClick={() => router.push(ROUTES.LOGIN)}
-      >
+      <Button className="self-center mt-10" onClick={() => router.push(ROUTES.LOGIN)}>
         Login
       </Button>
 
-      <div className="text-content-body text-center text-[13px] mt-20 font-normal">
-        © Success {currentYear}
-      </div>
+      <div className="text-content-body text-center text-[13px] mt-20 font-normal">© Success {currentYear}</div>
     </div>
   );
 };
