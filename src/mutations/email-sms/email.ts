ffc7b3bbe1b842b89f33eb1c24ce95e1e3230208
bipinparajuli <bipinprjl@gmail.com>
2025-09-03 09:43:26 +0545

    fix:only show default columns in lead table

--- src/mutations/email-sms/email.ts
+++ src/mutations/email-sms/email.ts
@@ -1,3 +1,4 @@
+import { useToastContext } from '@/context/toast-context';
 import { emailSmsApi } from '@/lib/emailSmsapi';
 import { LeadSchemaType } from '@/schema/lead-schema';
 import { SendEmailSchemaType } from '@/schema/send-email-schema';
@@ -9,7 +10,14 @@ const sendEmail = async (payload: SendEmailSchemaType) => {
 };
 
 export const useSendEmail = () => {
+  const { success, error } = useToastContext();
   return useMutation({
     mutationFn: sendEmail,
+    onSuccess: () => {
+      success('Email sent successfully');
+    },
+    onError: () => {
+      error('Email sending failed');
+    },
   });
 };
