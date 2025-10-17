bfb487e9b1486d79b1fafbb9b744b43cb19dc945
pokhrelgopal <pokhrelgopal27@gmail.com>
2025-10-17 09:30:11 +0545

    fix: conflict

--- src/schema/auth/forgot-password-schema.ts
+++ src/schema/auth/forgot-password-schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const forgotPasswordSchema = z.object({
   email: z
     .string({
-      required_error: 'Email is required',
+      error: 'Email is required',
     })
     .email('Invalid email address'),
 });
